5676bb5c8bcefdf0d2105a53e00b05259c66ba27
App Academy Student <student@appacademy.io>
2017-08-24 12:04:11 -0700

    Create Users model, mmigration

---
+++ 99 Cats Auth/app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :user_name, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  def self.reset_session_token!
+    session_token = SecureRandom::urlsafe_base64(16)
+  end
+end

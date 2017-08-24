89ac48b8ad1a45f19cb93e18e80d58b7cefaeb99
App Academy Student <student@appacademy.io>
2017-08-24 13:41:51 -0700

    Add User model

--- 99 Cats Auth/app/models/user.rb
+++ 99 Cats Auth/app/models/user.rb
@@ -14,7 +14,26 @@ class User < ApplicationRecord
   validates :user_name, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
+  def self.find_by_credentials(username, password)
+    user = User.find_by(user_name: username)
+    return user if user & user.is_password?(password)
+    nil
+  end
+
   def self.reset_session_token!
     session_token = SecureRandom::urlsafe_base64(16)
   end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  # def user_params
+  #   params.require(:user).permit(:user_name, :password)
+  # end
+
 end
